6f12673539866a44d867ec933de717ec4711a87e
edumaldonado <edumaldonado@gmail.com>
2022-10-03 17:08:36 -0300

    fin dia trabajo 2022-10-03

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Bootstrap from "./components/Bootstrap";
+
 
 function App() {
   return (
     <div className="App">
-
+      <h1>Frameworks CSS con React </h1>
+      <Bootstrap/>
     </div>
   );
 }
